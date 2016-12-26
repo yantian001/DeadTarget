305c2068ba1c388ed3758baaeb7c92947b9549c5
yantian001@vip.qq.com <yantian001@vip.qq.com>
2016-12-26 16:32:27 +0800

    更新UI前

--- Assets/Scripts/FProject/GamePlay/DSystem.cs
+++ Assets/Scripts/FProject/GamePlay/DSystem.cs
@@ -26,18 +26,6 @@ namespace FProject
                 Destroy(this);
             }
         }
-
-        // Use this for initialization
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
     }
 
 }
